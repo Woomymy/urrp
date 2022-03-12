881d08645bb28306bd403b9041bd48c18137b917
Woomy <woomy@woomy.be>
2022-03-12 11:05:39 +0100

    Page: Add sha256 sums in download buttons

--- lib/util.py
+++ lib/util.py
@@ -1,3 +1,4 @@
+from hashlib import sha256
 from os.path import exists
 from os import mkdir
 from subprocess import run
@@ -8,6 +9,7 @@ from lib.templates import get_template
 Various utilities
 """
 
+
 def check_files(files: list[str]):
     """
     Check if all required files exist
@@ -16,6 +18,7 @@ def check_files(files: list[str]):
         if not exists(file[1]):
             print(f"{file[0]} not found")
             exit(1)
+
 
 def clean_mkdir(path: str):
     """
@@ -26,6 +29,7 @@ def clean_mkdir(path: str):
         rmtree(path)
 
     mkdir(path)
+
 
 def mdToHtml(templateParams: dict[str, str], file: str) -> str:
     """
@@ -42,3 +46,20 @@ def mdToHtml(templateParams: dict[str, str], file: str) -> str:
         "-t",
         "html",
     ], capture_output=True, input=templateJinja.encode()).stdout.decode("UTF-8")
+
+
+def write_sha256(filepath: str):
+    """
+    Write files sha256 sum in {file}.sha256sum
+    """
+    out = f"{filepath}.sha256sum"
+    hash = sha256()
+
+    with open(filepath, "rb") as inputFile:
+        for bytes in iter(lambda: inputFile.read(4096), b""):
+            hash.update(bytes)
+
+    print(f"--- {filepath}.sha256: {hash.hexdigest()}")
+
+    with open(out, "w") as outFile:
+        outFile.write(f"{hash.hexdigest()}\n")
--- template.py
+++ template.py
@@ -3,14 +3,16 @@
 from shutil import copyfile, copytree
 from lib.config import load_config
 from lib.templates import get_template
-from lib.util import check_files, clean_mkdir, mdToHtml
+from lib.util import check_files, clean_mkdir, mdToHtml, write_sha256
+
 
 def main():
     config = load_config()
     recovery = "OrangeFox" if config.is_orangefox else "TWRP"
 
-    print(f"Generating {recovery} releases notes for {config.prettyname} ({config.device})")
-    
+    print(
+        f"Generating {recovery} releases notes for {config.prettyname} ({config.device})")
+
     # Check if all files exist
     check_files(
         [
@@ -22,10 +24,6 @@ def main():
     )
 
     clean_mkdir(config.out_dir)
-    
-    copyfile(config.zip, f"{config.out_dir}/install-{config.device}.zip")
-    copyfile(config.boot, f"{config.out_dir}/boot-{config.device}.img")
-
     templateParams = {
         "recovery": recovery,
         "marketname": config.prettyname,
@@ -36,21 +34,36 @@ def main():
     }
 
     # We can't directly add them to templateParams, because we need to pass templateParams to generate them
-    templateParams["chagenlogHtml"] = mdToHtml(templateParams, config.changelog_path)
-    templateParams["installInstructionsHtml"] = mdToHtml(templateParams, config.install_instructions_path)
+    templateParams["changelogHtml"] = mdToHtml(
+        templateParams, config.changelog_path)
+    templateParams["installInstructionsHtml"] = mdToHtml(
+        templateParams, config.install_instructions_path)
 
     template = get_template("templates/index.html.jinja2")
     index = template.render(templateParams)
 
     with open(f"{config.out_dir}/index.html", 'w') as indexfile:
         indexfile.write(index)
-    
-    for dir in ["style", "scripts"]:
-        print(f"-- Copying {dir} files")
-        copytree(dir, f"{config.out_dir}/{dir}")
+
+    copytree("style", f"{config.out_dir}/style")
+
+    zipOut = f"{config.out_dir}/install-{config.device}.zip"
+    bootOut = f"{config.out_dir}/boot-{config.device}.img"
+
+    print("-- Copying installer and boot image")
+    copyfile(config.zip, zipOut)
+    copyfile(config.boot, bootOut)
+
+    print("-- Generating SHA256 files")
+    write_sha256(zipOut)
+    write_sha256(bootOut)
 
     if not config.remove_vbmeta:
-        copyfile("vbmeta.img", f"{config.out_dir}/vbmeta.img")
+        print("-- Copying VBMeta and sha256sum")
+        vbmetaOut = f"{config.out_dir}/vbmeta.img"
+        copyfile("vbmeta.img", vbmetaOut)
+        write_sha256(vbmetaOut)
+
 
 if __name__ == "__main__":
     main()
